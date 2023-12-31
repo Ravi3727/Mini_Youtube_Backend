0296f35fc8f4f0cea94bcb42cea6ed82db3c222b
Ravi3727 <rk3727000@gmail.com>
2023-12-31 19:27:58 +0545

    Adding Multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET 
+});
+
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try{
+        if(!localFilePath) return null;
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resourse_type:"auto"
+        })
+        // file uploaded successfully
+        console.log("File uploaded successfully " + response.url);
+        return response.url;
+
+    }catch(e){
+        fs.unlinkSync(localFilePath)
+        //locally saved temporary file as the upload operation failed
+        return null;
+    }
+}
+
+
+
+export {uploadOnCloudinary};
